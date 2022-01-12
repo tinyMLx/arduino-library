854ed81d74ad1bda17bef25472ffa3782689db8f
ozanoner <ozan.oner@gmail.com>
2022-01-12 23:15:56 +0000

    custom board support

--- src/OV767X_TinyMLx.h
+++ src/OV767X_TinyMLx.h
@@ -8,19 +8,7 @@
 #define _OV767X_H_
 
 #include <Arduino.h>
-
-#define OV7670_VSYNC 8
-#define OV7670_HREF  A1
-#define OV7670_PLK   A0
-#define OV7670_XCLK  9
-#define OV7670_D0    10
-#define OV7670_D1    1
-#define OV7670_D2    0
-#define OV7670_D3    2
-#define OV7670_D4    3
-#define OV7670_D5    5
-#define OV7670_D6    6
-#define OV7670_D7    4
+#include "TinyMLx_board.h"
 
 enum
 {
